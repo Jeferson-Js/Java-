d455871c19eeb79663e2adf24bcdf051adef0752
Jeferson Oliveira <82729145+Jeferson-Js@users.noreply.github.com>
2022-02-12 11:34:53 -0300

    Casting em java

---
+++ casting.java
@@ -0,0 +1,13 @@
+public class casting {
+    public static void main(String[] args) {
+
+        int a, b;
+        a = 5;
+        b = 2;
+        double resultado;
+
+        resultado = (double)a / b;
+
+        System.out.println("O resultado é = " + resultado);
+    }
+}
