c56d7d8888160d5d66dc3069a27b144abfb1410e
Jeferson Oliveira <82729145+Jeferson-Js@users.noreply.github.com>
2022-02-15 08:07:34 -0300

    Estrutura de condição composta

---
+++ estruturacomp.java
@@ -0,0 +1,12 @@
+public class estruturacomp {
+    public static void main(String[] args){
+        int idade;
+        idade = 18;
+
+        if(idade < 18){
+            System.out.println("Se for maior de idade pode votar!");
+        }else {
+            System.out.println("Se não for maior de idade não pode votar!");
+        }
+    }
+}
