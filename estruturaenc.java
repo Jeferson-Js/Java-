5550de8edbb5425913dfa0e837b956cabb91ec8a
Jeferson Oliveira <82729145+Jeferson-Js@users.noreply.github.com>
2022-02-15 08:20:29 -0300

    Estrutura de condição encadeada.

---
+++ estruturaenc.java
@@ -0,0 +1,17 @@
+public class estruturaenc {
+    public static void main(String[] args){
+
+        String farol;
+
+        farol = "acao";
+
+        if(farol == "vermelho"){
+            System.out.println("Você deve parar!");
+        }else if(farol == "amarelo"){
+            System.out.println("Você deve reduzir a velocidade");
+        }else{
+            System.out.println("Você deve seguir em frente!");
+        }
+
+    }
+}
