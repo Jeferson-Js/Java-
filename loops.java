f07c10a410593074b84f88f555bd63d6d3214148
Jeferson Oliveira <82729145+Jeferson-Js@users.noreply.github.com>
2022-02-25 20:20:24 -0300

    Estrutura de repetição  com o for

---
+++ loops.java
@@ -0,0 +1,7 @@
+public class loops {
+    public static void main(String[] args){
+        for(int cont = 0; cont < 10; cont++){
+            System.out.println("Meu nome é Jaimes!");
+        }
+    }
+}
