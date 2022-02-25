07b10647323fb627b2b7a8c0ac95261a56abd671
Jeferson Oliveira <82729145+Jeferson-Js@users.noreply.github.com>
2022-02-25 15:59:14 -0300

    teste nota do aluno

---
+++ notaAluno.java
@@ -0,0 +1,13 @@
+public class notaAluno {
+    public static void main(String[] args){
+        
+        int nota = 65;
+        int media = 60;
+
+        if(nota > media ){
+            System.out.println("O aluno foi aprovado");
+        }else{
+            System.out.println("O aluno não foi aprovado");
+        }
+    }
+}
