a19d1d35db8e0f1251b581be09eb3f31dbc2e36d
Jeferson Oliveira <82729145+Jeferson-Js@users.noreply.github.com>
2022-02-26 09:09:44 -0300

    Alteração no código

--- arrays.java
+++ arrays.java
@@ -1,14 +1,19 @@
 public class arrays {
     public static void main(String[] args){
 
-        String[] frutas = new String[5];
+        String[] frutas = new String[6];
+        int[] numeros = {10,20,30,40,50};
 
-        frutas[0] = "goiaba";
+
+        frutas[0] = "Goiaba";
         frutas[1] = "Banana";
         frutas[2] = "Melancia";
         frutas[3] = "Amendoin";
         frutas[4] = "Uva";
+        frutas[5] = "Melao";
 
-        System.out.println(frutas[1]);
+        for(int i = 0; i <frutas.length; i++){
+            System.out.printf("%s - ", frutas[i]);
+        }
     }
 }
