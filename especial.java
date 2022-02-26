2ac80ecd5925e70535e8089814f9c4cfe50e2e08
Jeferson Oliveira <82729145+Jeferson-Js@users.noreply.github.com>
2022-02-26 09:27:16 -0300

    For especial

---
+++ especial.java
@@ -0,0 +1,11 @@
+public class especial {
+    public static void main(String[] args){
+
+        int[] num = {10,20,30,40,50,60,70,80,90,100};
+        String[] nomes = {"Jeferson", "Carol"};
+
+        for(String n:nomes){
+            System.out.printf("%s - ", n);
+        }
+    }
+}
