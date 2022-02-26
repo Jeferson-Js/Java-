c867e227d8507b407f430c6d856e874b602ce732
Jeferson Oliveira <82729145+Jeferson-Js@users.noreply.github.com>
2022-02-26 08:47:34 -0300

    Trabalhando com arrays em java

---
+++ arrays.java
@@ -0,0 +1,14 @@
+public class arrays {
+    public static void main(String[] args){
+
+        String[] frutas = new String[5];
+
+        frutas[0] = "goiaba";
+        frutas[1] = "Banana";
+        frutas[2] = "Melancia";
+        frutas[3] = "Amendoin";
+        frutas[4] = "Uva";
+
+        System.out.println(frutas[1]);
+    }
+}
